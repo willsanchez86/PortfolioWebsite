cc6544ffaf8ded33e01c0f896f53347ca3e712f3
Will <williamlancelotsanchez@gmail.com>
2022-06-15 13:43:32 -0400

    Open Graph Meta Tags Added in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,21 @@
 import Theme from '../styles/theme';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+        <Head>
+          <meta
+            property="og:title"
+            content="Developer Portfolio - William Sanchez"
+          />
+          <meta
+            property="og:description"
+            content="William Sanchez Full Stack Developer Portfolio. Review Projects, Languages, Technologies. Contact me directly. "
+          />
+          <meta property="og:image" content="/images/Portfolio Image.PNG" />
+        </Head>
         <Component {...pageProps} />
       </Theme>
     </>
